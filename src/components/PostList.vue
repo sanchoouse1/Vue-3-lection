a1afb1430b38356770647b753f8af07d14735be1
sanchoouse <sanchoouse@mail.ru>
2023-03-01 18:28:13 +0700

    Проект 3-х часовой лекции на Vue 3

---
+++ src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h3>Списсок пользователей</h3>
+        <post-item v-for="post in posts" :post="post" :key="post.id" remove="$emit('remove', post)"/>
+    </div>
+</template>
+
+<script>
+import PostItem from "@/components/PostItem.vue";
+export default {
+    components: {PostItem},
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+</style>
